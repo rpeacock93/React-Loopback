702b07873640c171cf44e4fdd025fb1a109b3a55
Ryan Peacock <rpeacock@learn4life.org>
2019-02-08 16:14:37 -0800

    first commit

---
+++ client/precache-manifest.d895bcf38b268b416deb9544ac69db0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c1176f2609448199957",
+    "url": "/static/js/main.6c1176f2.chunk.js"
+  },
+  {
+    "revision": "864213bfdb106c490be3",
+    "url": "/static/js/1.864213bf.chunk.js"
+  },
+  {
+    "revision": "6c1176f2609448199957",
+    "url": "/static/css/main.d764d451.chunk.css"
+  },
+  {
+    "revision": "d82328e102587335e183d65744130c07",
+    "url": "/index.html"
+  }
+];
